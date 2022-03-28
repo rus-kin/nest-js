d7f9ad599ce8b843382037df747f057dc5563edf
Ruslan Kinzebaev <rkinzebaev@intas-company.com>
2022-03-28 15:34:34 +0500

    init

---
+++ src/users/users.module.ts
@@ -0,0 +1,24 @@
+import {forwardRef, Module} from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./users.model";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "./user-roles.model";
+import {RolesModule} from "../roles/roles.module";
+import {AuthModule} from "../auth/auth.module";
+import {Post} from "../posts/posts.model";
+
+@Module({
+    providers: [UsersService],
+    controllers: [UsersController],
+    imports: [
+        SequelizeModule.forFeature([User, Role, UserRoles, Post]),
+        RolesModule,
+        forwardRef(() => AuthModule)
+    ],
+    exports: [
+        UsersService
+    ]
+})
+export class UsersModule {}
